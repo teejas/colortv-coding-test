5b1f6a2486dab2fa11be60043af989a2a26ab88a
Tejas Siripurapu <tejas97siripurapu@gmail.com>
2020-08-13 10:42:54 -0700

    Delete secrets.js

--- src/secrets.js
+++
@@ -1,4 +0,0 @@
-export const unsplashCredentials = {
-  access_key: 'aa2f3c3be8125f1fc86e3007153420c4e446c19b7b0c6d80a6257b281c9a0dc5',
-  secret_key: 'a5ab4ed2efdc772dca8d5636a26c0d897907df38cd92baa9067e57093d9596b5'
-}
